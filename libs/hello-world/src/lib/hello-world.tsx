9134672723f7e4723cbfe7af387b9eab6476592d
Abel Perez <abelper54@gmail.com>
2023-02-20 15:22:14 -0400

    first-commit

---
+++ libs/hello-world/src/lib/hello-world.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface HelloWorldProps {}
+
+export function HelloWorld(props: HelloWorldProps) {
+  return (
+    <div>
+      <h1>Welcome to obs-study-app!</h1>
+    </div>
+  );
+}
+
+export default HelloWorld;
