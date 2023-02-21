e7ca475cbec6c5e6c4d0f4b4c29a6e2562276120
Abel Perez <abelper54@gmail.com>
2023-02-21 18:54:02 -0400

    added next-pwa

--- apps/obs-study/next.config.js
+++ apps/obs-study/next.config.js
@@ -3,15 +3,22 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const { withNx } = require('@nrwl/next/plugins/with-nx');
 
+const withPWA = require('next-pwa')({
+  disable: process.env.NODE_ENV === 'development',
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+});
+
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
-const nextConfig = {
+const nextConfig = withPWA({
   nx: {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
-};
+});
 
 module.exports = withNx(nextConfig);
