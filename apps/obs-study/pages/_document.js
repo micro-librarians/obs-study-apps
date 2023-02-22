a1928d8cddb0ed702a18e9b6f9663c744a41bd1e
Abel Perez <abelper54@gmail.com>
2023-02-21 22:55:22 -0400

    added favicon

--- apps/obs-study/pages/_document.js
+++ apps/obs-study/pages/_document.js
@@ -1,11 +1,31 @@
-import { Html, Head, Main, NextScript } from 'next/document';
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html>
       <Head>
         <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon.png"></link>
+        <link rel="apple-touch-icon" href="/icon.png" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/favicon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        ></link>
+        <meta name="msapplication-TileColor" content="#ffffff" />
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
         <meta name="theme-color" content="#fff" />
       </Head>
       <body>
@@ -13,5 +33,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
