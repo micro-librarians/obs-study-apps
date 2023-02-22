a26300070d46e30c1402b411e40e4031b8a0749a
kintsoogi <kintsoogii@proton.me>
2023-02-22 14:06:46 -0600

    Added mounting option for Language Select component

--- apps/obs-study/pages/index.jsx
+++ apps/obs-study/pages/index.jsx
@@ -1,12 +1,12 @@
-import {useState} from 'react';
-import styled from '@emotion/styled';
-import { LanguageSelect } from 'language-select';
-import { ObsFrame } from 'obs-frame';
+import { useState, useRef, useEffect } from 'react'
+import styled from '@emotion/styled'
+import { LanguageSelect, mount } from 'language-select'
+import { ObsFrame } from 'obs-frame'
 
 const StyledPage = styled.div`
   .page {
   }
-`;
+`
 
 export function Index() {
   /*
@@ -18,18 +18,29 @@ export function Index() {
   const parseSelectedLanguage = (langObject) => {
     setUrl(langObject.zipball_url)
   }
+
+  const LanguageSelectComponent = () => {
+    const ref = useRef(null)
+
+    useEffect(() => {
+      mount(ref.current, { onLanguageSelect: parseSelectedLanguage })
+    })
+
+    return <div ref={ref} />
+  }
+
   return (
     <StyledPage>
       <div className="wrapper">
         <div className="container">
           <div id="welcome">
-            <LanguageSelect selectedLanguage={parseSelectedLanguage} />
+            <LanguageSelectComponent />
             <ObsFrame url={url} />
           </div>
         </div>
       </div>
     </StyledPage>
-  );
+  )
 }
 
-export default Index;
+export default Index
--- libs/language-select/src/index.js
+++ libs/language-select/src/index.js
@@ -1,1 +1,9 @@
-export * from './lib/language-select';
+import { createRoot } from 'react-dom/client'
+import LanguageSelect from './lib/language-select'
+
+// Mount function to start up the app
+const mount = (el, { onLanguageSelect }) => {
+  createRoot(el).render(<LanguageSelect selectedLanguage={onLanguageSelect} />)
+}
+
+export { mount, LanguageSelect }
