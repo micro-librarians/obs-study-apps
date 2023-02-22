a46c42697d2d7dbd64d8bbf844d62eca44a666fb
Abel Perez <abelper54@gmail.com>
2023-02-22 00:17:32 -0400

    caching-fetch-requets-setup

--- apps/obs-study/next.config.js
+++ apps/obs-study/next.config.js
@@ -1,14 +1,16 @@
 //@ts-check
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { withNx } = require('@nrwl/next/plugins/with-nx');
+const customCache = require('./cache')
+const { withNx } = require('@nrwl/next/plugins/with-nx')
 
 const withPWA = require('next-pwa')({
   disable: process.env.NODE_ENV === 'development',
   dest: 'public',
   register: true,
   skipWaiting: true,
-});
+  runtimeCaching: customCache,
+})
 
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
@@ -19,6 +21,6 @@ const nextConfig = withPWA({
     // See: https://github.com/gregberge/svgr
     svgr: false,
   },
-});
+})
 
-module.exports = withNx(nextConfig);
+module.exports = withNx(nextConfig)
