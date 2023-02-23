93364f358d51a8aa804302ace4c5428e0dc1bf8e
valyukhov <alexzed@bk.ru>
2023-02-23 14:51:03 +0300

    add list cached

--- apps/obs-study/pages/study/[owner]/[repo]/[ref].jsx
+++ apps/obs-study/pages/study/[owner]/[repo]/[ref].jsx
@@ -3,26 +3,23 @@ import { ObsFrame } from 'obs-frame'
 
 import styled from '@emotion/styled'
 
-
 const StyledPage = styled.div`
   .page {
   }
 `
-
 export function Index() {
   const {
     query: { owner, repo, ref },
   } = useRouter()
-
   return (
     <StyledPage>
       <div className="wrapper">
         <div className="container">
           <div id="welcome">
-            <ObsFrame
+            {owner && repo &&  <ObsFrame
               url={`https://git.door43.org/${owner}/${repo}/archive/master.zip`}
-              ref={ref}
-            />
+              startReference={ref}
+            />}
           </div>
         </div>
       </div>
